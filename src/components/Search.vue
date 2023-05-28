f64f028ac02bfc3813b01be18669a20fc755843d
David Silva <contatodavidmaia@gmail.com>
2023-05-28 13:27:48 -0300

    fix: add button on search input

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,31 +1,46 @@
 <script lang="ts" setup>
   import { ref } from 'vue'
-  import { Search } from 'lucide-vue-next'
+  import { ArrowRight, Search } from 'lucide-vue-next'
   import { useWeather } from '../stores'
 
   const search = ref('')
 
   const { getWeatherData } = useWeather()
 
-  function handleSearch(e: KeyboardEvent) {
-    if (e.key === 'Enter') {
-      getWeatherData(search.value)
-    }
+  function handleSearch() {
+    getWeatherData(search.value)
   }
 </script>
 
 <template>
   <div
-    class="flex w-64 items-center gap-3 rounded-lg border border-tuna-800 bg-tuna-900 p-2 font-medium text-tuna-100 transition-colors placeholder:text-tuna-300 [&:has(input:focus)]:border-tuna-500"
+    class="relative flex w-64 items-center gap-3 rounded-lg border border-tuna-800 bg-tuna-900 p-2 font-medium text-tuna-100 transition-colors placeholder:text-tuna-300 [&:has(input:focus)]:border-tuna-500"
   >
-    <Search class="w-5" :class="[search ? 'text-tuna-100' : 'text-tuna-300']" />
+    <Search
+      class="w-5 max-sm:w-6"
+      :class="[search ? 'text-tuna-100' : 'text-tuna-300']"
+    />
 
     <input
       v-model="search"
-      @keydown="handleSearch"
+      @keydown="
+        ({ key }) => {
+          if (key === 'Enter') {
+            handleSearch()
+          }
+        }
+      "
       class="bg-transparent outline-none"
       placeholder="Pesquise uma cidade."
       type="text"
     />
+
+    <button
+      @click="handleSearch"
+      class="absolute right-0 w-fit rounded-lg bg-ice-200 px-3 py-2 text-tuna-950 disabled:opacity-50"
+      :disabled="!!!search"
+    >
+      <ArrowRight class="w-4" />
+    </button>
   </div>
 </template>
