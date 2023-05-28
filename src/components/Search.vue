8390d1f764382bbb71121b40374dc1d74039e091
David Silva <contatodavidmaia@gmail.com>
2023-05-28 15:25:15 -0300

    fix: fixed search icon width

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -17,7 +17,7 @@
     class="relative flex w-64 items-center gap-3 rounded-lg border border-tuna-800 bg-tuna-900 p-2 font-medium text-tuna-100 transition-colors placeholder:text-tuna-300 [&:has(input:focus)]:border-tuna-500"
   >
     <Search
-      class="w-5 max-sm:w-6"
+      class="w-5 flex-shrink-0 max-sm:w-6"
       :class="[search ? 'text-tuna-100' : 'text-tuna-300']"
     />
 
